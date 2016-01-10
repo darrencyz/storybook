615aa66a52f3f11fddfaa0e6cc2b9a87bfb8160d
Darren Cao <darrencaoyz@gmail.com>
2016-01-10 02:47:37 -0500

    redesign home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many  :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
   attr_accessor :remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
