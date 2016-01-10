1890cb97a76e446223e5b34772eb1de35d89d5dc
Darren Cao <darrencaoyz@gmail.com>
2016-01-10 04:07:45 -0500

    landing image fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+User.create!(name:  "Darren Cao",
+             email: "darren@storybook.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@storybook.com"
   password = "password"
   User.create!(name:  name,
                email: email,
@@ -22,7 +22,7 @@ User.create!(name:  "Example User",
                password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
+users = User.order(:created_at).take(30)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
